a003aa679732d94a459dd3fa3fae3a6281f6e422
Rafael Neves <rafaelneves0405@gmail.com>
2025-09-25 23:16:14 -0300

    style: estilizado usuarios

---
+++ app/javascript/controllers/mask_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="mask"
+export default class extends Controller {
+  cpf(event) {
+    let value = event.target.value
+    value = value.replace(/\D/g, "")
+    value = value.replace(/(\d{3})(\d)/, "$1.$2")
+    value = value.replace(/(\d{3})(\d)/, "$1.$2")
+    value = value.replace(/(\d{3})(\d{3})$/, "$1-$2")
+    event.target.value = value.slice(0, 14)
+  }
+
+  phone(event) {
+    let value = event.target.value
+    value = value.replace(/\D/g, "")
+    value = value.replace(/^(\d{2})(\d)/, "($1) $2")
+    if (value.length > 13) {
+      // Handle 9-digit mobile numbers
+      value = value.replace(/(\d{5})(\d)/, "$1-$2")
+    } else {
+      // Handle 8-digit landlines
+      value = value.replace(/(\d{4})(\d)/, "$1-$2")
+    }
+    event.target.value = value.slice(0, 15)
+  }
+}
