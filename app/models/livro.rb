2fa0320df5036d879683c3d6b73a29f76557df25
Rafael Neves <rafaelneves0405@gmail.com>
2025-09-25 22:33:54 -0300

    style: estilizado livros

--- app/models/livro.rb
+++ app/models/livro.rb
@@ -4,4 +4,6 @@ class Livro < ApplicationRecord
   enum :status, { disponivel: 0, emprestado: 1 }, default: :disponivel
 
   has_many :emprestimos, dependent: :destroy
+
+  validates :observacoes, length: { maximum: 300 }
 end
